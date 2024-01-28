bd9ec3d2adb2be5fe7b8e7fd7634c7c515ae35c1
ahkubek <120653318+ahkubek@users.noreply.github.com>
2024-01-28 18:11:59 +0300

    getter

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+        public static void main(String[] args) {
+
+            SomeClass object= new SomeClass();
+            System.out.println(object.getStr());
+            object.setStr("dadon");
+            System.out.println(object.getStr());
+        }
+}
---
+++ src/SomeClass.java
@@ -0,0 +1,13 @@
+     class SomeClass {
+    private   String str = "hello world!!";
+    public  String getStr(){
+        return  str;
+    }
+
+    public void setStr(String newStr) {
+        this.str = newStr;
+    }
+
+
+
+}
